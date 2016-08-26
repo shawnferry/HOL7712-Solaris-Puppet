3b70c121754d040d45a50054e9a6f6130962fcc1
Shawn Ferry <shawn.ferry@oracle.com>
2016-08-26 10:29:35 -0700

    Update to work from a clean system
    puppet apply setup.pp should now correctly execute all
    setup steps on a clean system
    
    Fixed Vundle install
    Added Environemnt
    Added note about setting proxies for gem provider

--- setup.pp
+++ setup.pp
@@ -5,7 +5,18 @@
 
 # Install additional packages
 
-$labdir = '/root/HOL7712-Solaris-Puppet'
+$lab_homedir = '/root'
+$labdir = "${lab_homedir}/HOL7712-Solaris-Puppet"
+
+# If you need to define proxies here to reach outside resources
+# you must also add them to your environemnt to install gems via the
+# package provider. The package type doesn't recognize environment
+$environment = [
+  # HOME needs to be defined for Vundle install
+  "HOME=${lab_homedir}",
+  #'http_proxy=',
+  #'https_proxy='
+]
 
 $packages = [ 'git', 'editor/vim' ]
 package { $packages:
@@ -20,43 +31,50 @@ package { 'puppet-lint':
 
 # Make puppet-lint available on our path
 file_line { 'ruby_bin_path':
-  path => '/root/.profile',
+  path => "${lab_homedir}/.profile",
   line => 'export PATH=$PATH:/usr/ruby/2.1/bin';
 }
 
 # Copy the lab .vimrc to /root
-file { '/root/.vimrc':
-  ensure => present,
-  source => "${labdir}/labfiles/vimrc";
+file {
+  "${lab_homedir}/.vimrc":
+    ensure => present,
+    source => "${labdir}/labfiles/vimrc";
+
+  "${lab_homedir}/.zshrc":
+    ensure => present,
+    source => "${labdir}/labfiles/zshrc";
 }
 
 # Copy the lab .vimrc to /root
-file { '/root/invalid.pp':
+file { "${lab_homedir}/invalid.pp":
   ensure => present,
   source => "${labdir}/labfiles/invalid.pp";
 }
 
 # Ensure the bundle directory exists
-file { '/root/.vim/bundle':
+# There is no -p option we need the parents to exist as well
+file { ["${lab_homedir}/.vim/","${lab_homedir}/.vim/bundle"]:
   ensure => directory;
 }
 
 # Install Vundle
 # using the vcsrepo module would be better
 exec { 'vundle install':
-  command => '/usr/bin/git clone https://github.com/VundleVim/Vundle.vim.git /root/.vim/bundle/Vundle.vim',
-  creates => '/root/.vim/bundle/Vundle.vim',
-  # Do you need proxies?
-  # environment => '';
+  command     => "/usr/bin/git clone \
+    https://github.com/VundleVim/Vundle.vim.git \
+    ${lab_homedir}/.vim/bundle/Vundle.vim",
+  creates     => "${lab_homedir}/.vim/bundle/Vundle.vim",
+  environment => $environment,
+  before      => Exec['vundle plugins'];
 }
 
 # Install Vundle Plugins
 # This is not a good example
 exec { 'vundle plugins':
-  command => '/usr/bin/vim -i NONE -c VundleInstall -c quitall',
-  creates => '/root/.vim/bundle/vim-puppet',
-  # Do you need proxies?
-  # environment => '';
+  command     => '/usr/bin/vim -i NONE -c VundleInstall -c quitall',
+  creates     => "${lab_homedir}/.vim/bundle/vim-puppet",
+  environment => $environment;
 }
 
 # Create the manifests directory
